eb2a056f8914117b0281b26d7f60a375eb0e57d5
Sumit Shrirange <sumitshrirange81@gmail.com>
2026-02-11 12:57:35 +0530

    income model commit

---
+++ controllers/incomeController.js
@@ -0,0 +1,11 @@
+// Add income source
+exports.addIncome = async (req, res) => {};
+
+// Get All income source
+exports.getAllIncome = async (req, res) => {};
+
+// Delete income source
+exports.deleteIncome = async (req, res) => {};
+
+// Download Income in Excel
+exports.downloadIncomeExcel = async (req, res) => {};
