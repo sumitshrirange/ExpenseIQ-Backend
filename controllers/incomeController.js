80e5b120c4d298aa24d2bf6a4bf50808a00ab811
Sumit Shrirange <sumitshrirange81@gmail.com>
2026-02-11 13:25:22 +0530

    add income api commit

--- controllers/incomeController.js
+++ controllers/incomeController.js
@@ -1,5 +1,32 @@
+const User = require("../models/User");
+const Income = require("../models/Income");
+
 // Add income source
-exports.addIncome = async (req, res) => {};
+exports.addIncome = async (req, res) => {
+  const userId = req.user.id;
+
+  try {
+    const { icon, source, amount, date } = req.body;
+
+    // Validation: Check for missing fields
+    if (!source || !amount || !date) {
+      return res.status(400).json({ message: "All fields are required!" });
+    }
+
+    const newIncome = new Income({
+      userId,
+      icon,
+      source,
+      amount,
+      date: new Date(date),
+    });
+
+    await newIncome.save();
+    res.status(200).json(newIncome);
+  } catch (error) {
+    res.status(500).json({ message: "Server Error!" });
+  }
+};
 
 // Get All income source
 exports.getAllIncome = async (req, res) => {};
