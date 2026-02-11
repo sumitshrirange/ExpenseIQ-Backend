d9a6e7c7c3838aa68f15175f4c470e3812c8d07d
Sumit Shrirange <sumitshrirange81@gmail.com>
2026-02-11 13:59:24 +0530

    download income excel api commit

--- controllers/incomeController.js
+++ controllers/incomeController.js
@@ -1,4 +1,4 @@
-const User = require("../models/User");
+const xlsx = require("xlsx");
 const Income = require("../models/Income");
 
 // Add income source
@@ -51,4 +51,25 @@ exports.deleteIncome = async (req, res) => {
 };
 
 // Download Income in Excel
-exports.downloadIncomeExcel = async (req, res) => {};
+exports.downloadIncomeExcel = async (req, res) => {
+  const userId = req.user.id;
+
+  try {
+    const income = await Income.find({ userId }).sort({ date: -1 });
+
+    // Prepare data for Excel
+    const data = income.map((item) => ({
+      Source: item.source,
+      Amount: item.amount,
+      Date: item.date,
+    }));
+
+    const wb = xlsx.utils.book_new();
+    const ws = xlsx.utils.json_to_sheet(data);
+    xlsx.utils.book_append_sheet(wb, ws, "Income");
+    xlsx.writeFile(wb, "income_details.xlsx");
+    res.download("income_details.xlsx");
+  } catch (error) {
+    res.status(500).json({ message: "Server Error!" });
+  }
+};
