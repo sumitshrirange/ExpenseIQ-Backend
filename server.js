705bb1cd88451ac172026a12c79c6ad38d3243ae
Sumit Shrirange <sumitshrirange81@gmail.com>
2026-02-15 05:46:50 +0530

    error solved commit

--- server.js
+++ server.js
@@ -41,3 +41,5 @@ app.use("/uploads", express.static("uploads"));
 
 // const PORT = process.env.PORT || 5000;
 // app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
+
+module.exports = app;
