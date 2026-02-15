376feefdd1169d53a52b2646af8cfd363abacdfb
Sumit Shrirange <sumitshrirange81@gmail.com>
2026-02-15 13:40:18 +0530

    session added commit

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
+const session = require("express-session");
 const path = require("path");
 const connectDB = require("./config/db");
 const authRoutes = require("./routes/authRoutes");
